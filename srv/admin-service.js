74bc204cb7f15c99b722bdd4b204181ad30c57d9
Balbino Soares Ferreira Filho <c0002867@cenibra.com.br>
2019-03-12 15:02:15 -0300

    Adjust js files

--- srv/admin-service.js
+++ srv/admin-service.js
@@ -5,9 +5,9 @@ module.exports = (srv) => {
 
     const {Event} = cds.entities ('com.sap.sapmentors.sitregcapm')
   
-    // Reduce stock of ordered books
+
     srv.before ('CREATE', 'Events', async (req) => {
-      const Event = req.data
+     
 
     })
   
--- srv/organizer-service.js
+++ srv/organizer-service.js
@@ -1,15 +1,13 @@
 module.exports = (srv) => {
 
-    //const {Event} = cds.entities ('com.sap.sapmentors.sitregcapm')
-  
-    
+     
     srv.before ('CREATE', 'Events', async (req) => {
-        console.log('After Update')
+        
     })
   
     
     srv.before ('UPDATE', 'Events', each => {
-      console.log('Before Update')
+   
     })
   
   }
