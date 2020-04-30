85458b5462b02c5a386bc31161b25457f3783dde
DusanSacha <sacha.dusan@gmail.com>
2020-04-30 22:58:11 +0200

    Update package.json with axios

--- spec/sharedFunctions.js
+++ spec/sharedFunctions.js
@@ -2,16 +2,16 @@ const axios = require('axios')
 
 exports.createEvent = async (endpoint, data, user, done) => {
   const body = JSON.stringify(data)
-    axios.post(endpoint, body, {
-        auth: {username: user.auth.user},
-        headers: {
-          'content-type': 'application/json'
-        }
-      })
-      .then( response => {
-        expect(response.status).toEqual(201)
-        done()
-      }).catch( error => {
-        console.log(error);
-      })
+  axios.post(endpoint, body, {
+    auth: { username: user.auth.user },
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+    .then(response => {
+      expect(response.status).toEqual(201)
+      done()
+    }).catch(error => {
+      console.log(error)
+    })
 }
